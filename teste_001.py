a357b5e7f4d052a0b3a8091062d79c132bf5049e
Matheus L3 <m.sosinformatica@gmail.com>
2020-09-22 13:17:01 -0300

    commit 2

--- teste_001.py
+++ teste_001.py
@@ -1,2 +1,2 @@
-msg = input('Qual é o seu nome?')
-print('olá,',msg,', seja bem vindo aos testes iniciais de Python!')
+nome = input('Qual é o seu nome?')
+print('olá {}, seja bem vindo aos testes iniciais de Python!'.format(nome))
