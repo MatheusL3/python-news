8db93318199a6ebe1d031325bb57f850fd26134e
Matheus L3 <m.sosinformatica@gmail.com>
2020-09-22 13:32:16 -0300

    commit 3

---
+++ teste_002.py
@@ -0,0 +1,5 @@
+#========== SOMA DE DOIS NUMEROS ========
+s1 = int(input('Digite um numero:'))
+s2 = int(input('Digite outro numero:'))
+result = s1+s2
+print('A soma dos dois numeros é {}'.format(result))
