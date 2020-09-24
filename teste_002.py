1fee7ef346366555f75f39ba22da81ce541e1522
Matheus L3 <m.sosinformatica@gmail.com>
2020-09-24 13:27:43 -0300

    teste FOR

--- teste_002.py
+++ teste_002.py
@@ -1,11 +1,5 @@
 import random
-
-conta = 0
 nomes = []
-while (conta <= 5):
+for conta in range(0,5):
     nomes.insert(conta,str(input('Digite o nome do aluno numero {}:'.format(conta))));
-    conta += 1
-
-print('E o nome selecionado é... \n {}'.format(nomes[random.randint(0,6)]))
-
-
+print('E o nome selecionado é... \n {}'.format(nomes[random.randint(0,5)]))
